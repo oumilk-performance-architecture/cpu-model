c364b0c64124b49159abccab8a2d976ab58c7548
Jonathan Ou <oujonathan28@gmail.com>
2021-09-27 20:12:18 -0500

    Bus Fix
    Fixed bus fetch code

--- src/bus/bus.cpp
+++ src/bus/bus.cpp
@@ -2,21 +2,65 @@
 #include <iostream>
 #include <fstream> 
 #include <cstdint>
+#include <delay.h>
 
 #include "bus.h"
 
 #include <stdio.h> // Temporary
+
+Delay<uint32_t>* nip_fetch_mem_rp = new Delay<uint32_t>("nip_fetch_mem");
+Delay<uint32_t>* bus_return_info_wp = new Delay<uint32_t>("bus_return_info");
+
 Bus::Bus(unsigned int sram_size, unsigned int srom_size):
     sram(sram_size * 1024, 0),
     code(srom_size * 1024, 0)
 {
+   nip_fetch_mem_rp->SetLatency(3);
 }
 
 Bus::~Bus() {
 
 }
 
-uint8_t Bus::read8(unsigned int addr) {
+void Bus::Process(int cycle) {
+    cycle_ = cycle;
+
+    uint32_t pc;
+    while (nip_fetch_mem_rp->Receive(pc, cycle_)) {
+        bus_return_info_wp->Send(Read16(pc), cycle_);
+    }
+}
+
+// TODO: Need to check for correct memory access
+uint32_t Bus::Read32(uint32_t addr) {
+    uint32_t data;
+    data =  Read16(addr);
+    data |= Read16(addr + 2) << 16;
+    return data;
+};
+
+void Bus::Write32(uint32_t addr, uint32_t data) {
+    uint16_t tdata = (data & 0x0000FFFF);
+    Write16(addr, tdata);
+    tdata = (data & 0xFFFF0000) >> 16;
+    Write16(addr + 2, tdata);
+};
+ 
+uint16_t Bus::Read16(uint32_t addr) {
+    uint16_t data;
+    data =  Read8(addr);
+    data |= Read8(addr + 1) << 8;
+    return data;
+};
+
+void Bus::Write16(uint32_t addr, uint16_t data) {
+    uint8_t tdata = (data & 0x00FF); 
+    Write8(addr, tdata);
+    tdata = (data & 0xFF00) >> 16;
+    Write8(addr + 1, tdata);
+};
+
+uint8_t Bus::Read8(uint32_t addr) {
     unsigned int mask = (addr & 0xF0000000) >> 7;
     unsigned int base = 0;
     switch (mask) {
@@ -36,30 +80,27 @@ uint8_t Bus::read8(unsigned int addr) {
     return 0;
 };
 
-uint8_t Bus::write8(unsigned int addr, uint8_t data) {
-    unsigned int mask = (addr & 0xF0000000) >> 7;
-    unsigned int base = 0;
+void Bus::Write8(uint32_t addr, uint8_t data) {
+    uint32_t mask = (addr & 0xF0000000) >> 7;
+    uint32_t base = 0;
     switch (mask) {
         case 0x0 ... 0x1:
             base = 0;
             code[addr - (base << 7)] = data;
-            return data;
         case 0x2 ... 0x3:
             break;
         case 0x4 ... 0x5:
             base = 4;
             sram[addr - (base << 7)] = data;
-            return data;
         case 0x6 ... 0x9:
             break;
         case 0xA ... 0xD:
             break;
     }
-    return 0;
 };
 
 // Modifieds: Bus->code;
-void Bus::load_program(std::string filename) {
+void Bus::Load_program(std::string filename) {
     std::cout << "loading program: " << filename << "\n";
 
     FILE *fp = fopen(filename.c_str(), "rb");
@@ -68,14 +109,14 @@ void Bus::load_program(std::string filename) {
     
 };
 
-void Bus::output_contents(std::string filename) {
+void Bus::Output_contents(std::string filename) {
     std::ofstream outfile;
     unsigned int i = 0;
     // CODE
     outfile.open(filename + ".code.mem");
     for_each(code.begin(), code.end(), [&i, &outfile](uint8_t value) {
-        outfile << std::dec << i++ << " ";
-        outfile << std::dec << int(value) << "\n";
+        outfile << std::hex << i++ << " ";
+        outfile << std::hex << int(value) << "\n";
     });
     outfile.close();
 
--- src/bus/bus.h
+++ src/bus/bus.h
@@ -3,20 +3,33 @@
 
 #include <cstdint>
 #include <vector>
+#include <delay.h>
 
-#include "bus.h"
+
 /*
   Loads 1 byte per index
 */
 class Bus {
+    
     public:
         Bus(unsigned int sram_size, unsigned int srom_size);
         ~Bus(); 
-        uint8_t read8(unsigned int addr);
-        uint8_t write8(unsigned int addr, uint8_t data);
-        void output_contents(std::string filename);
-        void load_program(std::string filename);
+
+        void Process(int cycle);
+
+        uint32_t Read32(uint32_t addr);
+        void Write32(uint32_t addr, uint32_t data);
+
+        uint16_t Read16(uint32_t addr);
+        void Write16(uint32_t addr, uint16_t data);
+        
+        uint8_t Read8(uint32_t addr);
+        void Write8(uint32_t addr, uint8_t data);
+
+        void Output_contents(std::string filename);
+        void Load_program(std::string filename);
     private:
+        int cycle_;
         std::vector<uint8_t> sram;
         std::vector<uint8_t> code;
 };
